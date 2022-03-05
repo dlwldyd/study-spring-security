3dfe3086fdcbe086eccd91ca67b5a6d14b6140d6
dlwldyd <dlwldyd9303@naver.com>
2022-03-05 21:06:05 +0900

    로그아웃 처리 추가

--- core/src/main/java/springsecurity/core/controller/login/LoginController.java
+++ core/src/main/java/springsecurity/core/controller/login/LoginController.java
@@ -1,7 +1,13 @@
 package springsecurity.core.controller.login;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class LoginController {
@@ -9,5 +15,18 @@ public class LoginController {
     @GetMapping("/login")
     public String login() {
         return "user/login/login";
+    }
+
+    @GetMapping("logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null) {
+            //SecurityContextLogoutHandler 는 로그아웃을 처리하는 핸들러
+            //logout() 메서드를 호출하면 authentication 에 담긴 유저가 로그아웃됨
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+        return "redirect:/";
     }
 }
